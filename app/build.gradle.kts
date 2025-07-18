9d6a98cda9e3a951af3599ed16545fa28aa66d8b
ParkHyunSu99 <phs0767@pineone.com>
2025-07-18 13:21:03 +0900

    화면 구성하기 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.dagger.hilt.android)
+    alias(libs.plugins.kpat)
 }
 
 android {
@@ -43,9 +45,18 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
+    implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.constraintlayout)
+
+    implementation(libs.dagger.hilt)
+    kapt(libs.dagger.hilt.compiler)
+    implementation(libs.coroutine)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+kapt {
+    correctErrorTypes = true
 }
---
+++ app/src/main/java/fastcampus/aos/part3/part3_chapter7/class HiltApplication.kt
@@ -0,0 +1,8 @@
+package fastcampus.aos.part3.part3_chapter7
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltApplication : Application() {
+}
